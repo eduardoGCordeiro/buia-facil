2e805440280190c359e6f9d3a78356d5f785a9ca
Carlos-Lopes <hlopes.ec@gmail.com>
2017-08-01 16:26:17 -0300

    Corpo de Desenvolvimento do projeto Buia Facil Localização Laravel /
    Localização Angular
    /resources/assets/angular

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BuiaFacil\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'BuiaFacil\Model' => 'BuiaFacil\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
